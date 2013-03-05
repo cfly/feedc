0313cddf3c1de18684b0a1b74d8f5d6b0394bb28
caofei <cfly.hi@gmail.com>
2013-03-06 00:55:11 +0800

    mvn complie

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 #!/bin/env bash
+/home/cfly/apache-maven-3.0.5/bin/mvn complie -f /home/cfly/feedc/pom.xml
 /home/cfly/apache-maven-3.0.5/bin/mvn exec:java -Dexec.mainClass=org.caofei.feedc.App -f /home/cfly/feedc/pom.xml
