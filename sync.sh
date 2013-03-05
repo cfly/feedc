d41679c55099b301b1da96f71b7a62388214ded8
caofei <cfly.hi@gmail.com>
2013-03-06 01:02:44 +0800

    Signed-off-by: caofei <cfly.hi@gmail.com>

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 /home/cfly/apache-maven-3.0.5/bin/mvn compile -f /home/cfly/feedc/pom.xml
-/home/cfly/apache-maven-3.0.5/bin/mvn exec:java -Dexec.mainClass=org.caofei.feedc.App -f /home/cfly/feedc/pom.xml
+/home/cfly/apache-maven-3.0.5/bin/mvn exec:java -Dexec.mainClass=org.caofei.feedc.App -f /home/cfly/feedc/pom.xml -Dexec.arguments='http://cnbeta.com/backend.php,http://t66y.com/rss.php?fid=7'
