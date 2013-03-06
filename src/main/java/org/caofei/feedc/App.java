4765069bd6d7030040fe6efab1751283f3bd1e9b
cfly <cfly.hi@gmail.com>
2013-03-06 16:31:31 +0800

    test

--- src/main/java/org/caofei/feedc/App.java
+++ src/main/java/org/caofei/feedc/App.java
@@ -1,14 +1,22 @@
 package org.caofei.feedc;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
@@ -21,10 +29,12 @@ import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.CoreProtocolPNames;
 
+import com.sun.syndication.feed.synd.SyndContent;
 import com.sun.syndication.feed.synd.SyndEntry;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.FeedException;
 import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.SyndFeedOutput;
 import com.sun.syndication.io.XmlReader;
 
 /**
@@ -32,15 +42,18 @@ import com.sun.syndication.io.XmlReader;
  * 
  */
 public class App {
-	private static final Map<String,String> kv = new HashMap<String,String>();
-	static{
+	private static final Map<String, String> kv = new HashMap<String, String>();
+	private static final String TOKEN;
+	static {
 		ResourceBundle kvprop = ResourceBundle.getBundle("kv");
 		Enumeration<String> keys = kvprop.getKeys();
-		while(keys.hasMoreElements()){
+		while (keys.hasMoreElements()) {
 			String key = keys.nextElement();
 			kv.put(key, kvprop.getString(key));
 		}
+		TOKEN = kv.get("TOKEN");
 	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		for (int i = 0; i < args.length; i++) {
@@ -48,18 +61,6 @@ public class App {
 			System.out.println(kv.get(arg));
 			process(arg);
 		}
-//		try {
-//			List<String> lines = IOUtils.readLines(new FileReader("./index.rss"));
-//			for (String line : lines) {
-//				System.out.println(line);
-//			}
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 	private static void process(String rssUrl) {
@@ -67,7 +68,7 @@ public class App {
 			setProxy();
 			pullFeed(rssUrl);
 			genBean();
-			readablityAPI();
+			// readablityAPI();
 			genEntity();
 			persistence();
 		} catch (ClientProtocolException e) {
@@ -86,20 +87,73 @@ public class App {
 	}
 
 	private static void setProxy() throws MalformedURLException, IOException {
-//		 System.setProperty("http.proxyHost", "172.16.131.7");
-//		 System.setProperty("http.proxyPort", "3128");
-//		 System.setProperty("http.proxyHost", "172.16.131.7");
-//		 System.setProperty("http.proxyPort", "3128");
-//		 System.out.println(IOUtils.readLines(new
-//		 URL("http://cnbeta.com/backend.php").openStream()));
-//		 IOUtils.copy(new
-//				 URL("http://cnbeta.com/backend.php").openStream(),new FileWriter("./index.rss"));
-		 
+		System.setProperty("http.proxyHost", "172.16.131.7");
+		System.setProperty("http.proxyPort", "3128");
+		// System.setProperty("http.proxyHost", "172.16.131.7");
+		// System.setProperty("http.proxyPort", "3128");
+		// System.out.println(IOUtils.readLines(new
+		// URL("http://cnbeta.com/backend.php").openStream()));
+		// IOUtils.copy(new
+		// URL("http://cnbeta.com/backend.php").openStream(),new
+		// FileWriter("./index.rss"));
+
 	}
 
-	private static void readablityAPI() {
-		// TODO Auto-generated method stub
+	private static void pullFeed(String rssUrl) throws ClientProtocolException,
+			IOException, IllegalArgumentException, FeedException {
+		SyndFeedInput input = new SyndFeedInput();
+		SyndFeed feed = input.build(new XmlReader(new URL(rssUrl)));
+		System.out.println(feed.getTitle());
+		List<SyndEntry> entries = feed.getEntries();
 
+		for (int i = 0; i < entries.size(); i++) {
+			SyndEntry syndEntry = entries.get(i);
+			System.out.println("entries\t" + i + "/" + entries.size() + "\t" + syndEntry.getLink());
+			// }
+			// for(SyndEntry syndEntry : entries) {
+			SyndContent syndContent = syndEntry.getDescription();
+			List<String> lines = readablityAPI(syndEntry.getLink());
+			if(lines!=null && !lines.isEmpty()){
+				syndContent.setValue(lines.get(0));
+			}
+			List<SyndContent> contents = new LinkedList<SyndContent>();
+			contents.add(syndContent);
+			syndEntry.setContents(contents);
+		}
+
+		String fileStr = kv.get(rssUrl);
+		File file = new File(fileStr);
+		file.getParentFile().mkdirs();
+		file.createNewFile();
+			
+		Writer writer = new FileWriter(kv.get(rssUrl));
+		SyndFeedOutput output = new SyndFeedOutput();
+		output.output(feed, writer);
+		writer.close();
+		// SyndEntry entry = (SyndEntry) feed.getEntries().get(0);
+		// System.out.println(entry.getLink());
+	}
+
+	private static Pattern regx = Pattern.compile("<content>(.*)</content>",
+			Pattern.DOTALL);
+
+	private static List<String> readablityAPI(String url)
+			throws ClientProtocolException, IOException {
+		String api = "https://readability.com/api/content/v1/parser?token="
+				+ TOKEN + "&format=xml&url=";
+		List<String> lines = curl(api + url);
+		StringBuilder sb = new StringBuilder();
+		if (lines != null) {
+			for (String line : lines) {
+				sb.append(line);
+			}
+			lines.clear();
+			Matcher matcher = regx.matcher(sb.toString());
+			if (matcher.find()) {
+				lines.add(matcher.group(1));
+			}
+		}
+		return lines;
 	}
 
 	private static void persistence() {
@@ -117,16 +171,9 @@ public class App {
 
 	}
 
-	private static void pullFeed(String rssUrl) throws ClientProtocolException, IOException, IllegalArgumentException, FeedException {
-		SyndFeedInput input = new SyndFeedInput();
-//		String feedRoot = "http://cnbeta.com/backend.php";
-		//feedRoot = "http://t66y.com/rss.php?fid=7";
-		SyndFeed feed = input.build(new XmlReader(new URL(rssUrl)));
-		System.out.println(feed.getTitle());
-		SyndEntry entry =  (SyndEntry) feed.getEntries().get(0);
-		System.out.println(entry.getLink());
-		String u = "GET /api/content/v1/parser?url=http://blog.readability.com/2011/02/step-up-be-heard-readability-ideas/&token=1b830931777ac7c2ac954e9f0d67df437175e66e";
-		if(true)return;
+	private static List<String> curl(String url)
+			throws ClientProtocolException, IOException {
+		List<String> lines = null;
 		HttpHost proxy = new HttpHost("172.16.131.7", 3128, "http");
 		HttpClient httpclient = new DefaultHttpClient();
 		httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY,
@@ -135,24 +182,29 @@ public class App {
 				CoreProtocolPNames.HTTP_ELEMENT_CHARSET, "UTF-8");
 
 		// Prepare a request object
-		HttpGet httpget = new HttpGet("");
-		// HttpGet httpget = new HttpGet("http://baidu.com");
-
+		HttpGet httpget = new HttpGet(url);
+		
 		// Execute the request
 		HttpResponse response = httpclient.execute(httpget);
 
 		// Examine the response status
-		System.out.println(response.getStatusLine());
-		String encoding = response.getHeaders("Content-Type")[0].getElements()[0].getParameterByName("charset").getValue();
+		// System.out.println(response.getStatusLine());
+		if (response.getStatusLine().getStatusCode() != 200) {
+			System.err.println(response.getStatusLine());
+			return null;
+		}
+		String encoding = response.getHeaders("Content-Type")[0].getElements()[0]
+				.getParameterByName("charset").getValue();
 		// Get hold of the response entity
 		HttpEntity entity = response.getEntity();
 
+		// StringBuilder sb = new StringBuilder();
 		// If the response does not enclose an entity, there is no need
 		// to worry about connection release
 		if (entity != null) {
 			InputStream instream = entity.getContent();
 			try {
-				List<String> lines = IOUtils.readLines(instream,encoding);
+				lines = IOUtils.readLines(instream, encoding);
 				// BufferedReader reader = new BufferedReader(
 				// new InputStreamReader(instream));
 				// do something useful with the response
@@ -161,9 +213,10 @@ public class App {
 				// while ((line = reader.readLine()) != null) {
 				// System.out.println(line);
 				// }
-				for (String line : lines) {
-					System.out.println(line);
-				}
+				// for (String line : lines) {
+				// System.out.println(line);
+				// sb.append(line);
+				// }
 
 			} catch (IOException ex) {
 
@@ -191,6 +244,6 @@ public class App {
 			// immediate deallocation of all system resources
 			httpclient.getConnectionManager().shutdown();
 		}
-
+		return lines;
 	}
 }
